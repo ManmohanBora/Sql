492f0efd0f72713e2f2c8037b99440c41573ed08
Dishonoured <76729057+ManmohanBora@users.noreply.github.com>
2026-01-11 22:26:40 +0530

    Add files via upload

---
+++ Select Distinct.sql
@@ -0,0 +1,11 @@
+
+
+select * from employeerecords
+
+select distinct firstname from employeerecords
+
+select distinct CONCAT(firstname,' ',lastname) [Full Name]from employeerecords
+
+select distinct salary from employeerecords
+
+select distinct * from employeerecords
---
+++ Select Statement.sql
@@ -0,0 +1,11 @@
+
+
+select * from Employees
+
+select FirstName,Salary from Employees
+
+select EmployeeID,CONCAT(FirstName,' ',LastName) from Employees
+
+select EmployeeID,CONCAT(FirstName,' ',LastName) [Full Name] from Employees
+
+select EmployeeID,CONCAT(FirstName,' ',LastName) as [Full Name] from Employees

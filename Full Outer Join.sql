9208e0b7862d8e6196db6eeb0a832873a9712464
Dishonoured <76729057+ManmohanBora@users.noreply.github.com>
2026-01-30 19:15:14 +0530

    Add files via upload

---
+++ Full Outer Join.sql
@@ -0,0 +1,14 @@
+
+
+select * from table1
+
+select * from table2
+
+select * from table1 full outer join table2
+on table1.c1 = table2.C1
+
+select * from table1 a full outer join table2 b
+on a.c1 = b.C1
+
+select a.c1,a.c2,b.C3 from table1 a full outer join table2 b
+on a.C1 = b.C1
